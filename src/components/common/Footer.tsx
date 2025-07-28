128d01ebb9a6a20216246ddd7caf5dbddb4a1ae7
OhDongI <odi0716@naver.com>
2025-07-28 19:03:17 +0900

    feat: footer 구현 (#6)

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,30 @@
+import styles from "../../styles/components/common/Footer.module.css";
+
+export default function Footer(){
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <div className={styles.top}>
+          <img src="/logo.svg" alt="LearnIT Logo" className={styles.logo} />
+          <div>
+            <div className={styles.bar}></div> LearnIT 
+            <div className={styles.bar}></div> Learn it with IT - IT를 통해 함께 배우다
+          </div>
+        </div>
+        <hr></hr>
+
+        <div className={styles.bottom}>
+          <div>
+            <p>(주)런잇 | 대표: 러닝크루</p>
+            <p>사업자등록번호: 123-45-67890</p>
+            <p>주소: 서울특별시 서대문구 거북골로 34</p>
+            <p>이메일: 1234@learnit.co.kr</p>
+          </div>
+          <div>@2025 LearnIt</div>
+        </div >
+
+      </div>
+    </footer>
+
+    );
+}
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,16 +1,25 @@
+import Footer from "../common/Footer";
 import Navbar from "../common/Navbar"; 
+import React from "react";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
+
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <>
+    <div style={{
+      display: "flex",
+      flexDirection: "column",
+      minHeight: "100vh"
+    }}>
       <Navbar />
-      {/* header and gnb */}
-      <div>{children}</div>
-      {/* footer */}
-    </>
-  )
-}
+      <main style={{ flex: 1 }}>
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
 export default Layout;
