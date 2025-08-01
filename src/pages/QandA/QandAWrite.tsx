9f587c37b80a4c6dc16f53420c3caea91e4a058e
soyeon <soyeon20031010@gmail.com>
2025-08-01 16:15:45 +0900

    feat: QandA write page 구현

--- src/pages/QandA/QandAWrite.tsx
+++ src/pages/QandA/QandAWrite.tsx
@@ -1,7 +1,10 @@
+import { ContentEditor } from "../../components/common/ContentEditor"
+import styles from "../../styles/QandA/QandAWrite.module.css"
 
 export const QandAWrite = () => {
   return (
-    <>
-    </>
+    <div className={styles.write__container}>
+      <ContentEditor contentText="질문 내용" wholeTitle="Q&A 작성하기" />
+    </div>
   )
 }
