6d076bd47a00e016cba714c6fea7cbad9490d3fa
soyeon <soyeon20031010@gmail.com>
2025-08-03 19:37:34 +0900

    feat: api 연결

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,19 @@
+import { nicknameConfirm } from '../../apis/login/login';
 import styles from '../../styles/pages/home/HomePageStyle.module.css';
 
 export const Home = () => {
+
+  const handleNicknameConfirm = async () => {
+    try {
+      const response = await nicknameConfirm("test");
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
   return (
     // 사실 module css 쓰면 bem 방법론 안써도 될 것 같긴합니당 본인이 사용하기에 편한걸로 사용하세여
-    <div className={styles.homePage__container}>
+    <div className={styles.homePage__container} onClick={handleNicknameConfirm}>
       <p className={styles.homePage__title}>Home</p>
       <p className={styles.homePage__subTitle}>Home1</p>
       <p className={styles.homePage__subTitle2}>Home2</p>
