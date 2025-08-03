1de6c7231b2a8a3274fca605430b394204db3e14
JunHyuck Lee <jjunjjun3@gmail.com>
2025-08-03 23:16:04 +0900

    feat: Router 수정 작업 중

--- src/Router.tsx
+++ src/Router.tsx
@@ -9,6 +9,9 @@ import HostGroupStudyWriting from "./components/hostGroupStudyWriting/HostGroupS
 import HostGroupStudy from "./components/hostGroupStudy/HostGroupStudy"
 import HostGroupParticipants from "./components/hostGroupParticipants/HostGroupParticipants"
 import HostGroupApplicant from "./components/hostGroupApplicant/HostGroupApplicant"
+import Menu from "./components/fixedGroupHeader/Menu"
+import Step from "./components/fixedGroupHeader/Step"
+import FixedBanner from "./components/fixedGroupHeader/FixedBanner"
 import { CreateGroup } from "./pages/createGroup/CreateGroup"
 import { ShareNoteList } from "./pages/shareNote/ShareNoteList"
 import { ShareNoteDetail } from "./pages/shareNote/ShareNoteDetail"
@@ -20,6 +23,9 @@ export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/FixedBanner" element ={<FixedBanner />} />
+        <Route path="/Step" element ={<Step totalSteps={7} currentStep={1} />} />
+        <Route path="/Menu" element ={<Menu />} />
         <Route path="/HostGroupApplicant" element ={<HostGroupApplicant />} />
         <Route path="/HostGroupParticipants" element ={<HostGroupParticipants />} />
         <Route path="/HostGroupStudy" element ={<HostGroupStudy />} />
