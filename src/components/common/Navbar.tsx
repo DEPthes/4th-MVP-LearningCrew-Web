e1d4062c4da39ff8878f317ba5093c62ee1140df
OhDongI <odi0716@naver.com>
2025-08-14 19:13:01 +0900

    feat: /group/* 경로에서 '내 그룹' 탭 활성화 (#52)

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -14,24 +14,60 @@ export default function Navbar() {
     });
   };
 
+  const isMyGroupActive =
+    pathname === "/mygroup" || pathname.startsWith("/group/");
+
   return (
     <>
       <div style={{ height: navbarHeight }}></div>
+
       <nav className={styles.navbar}>
         <div className={styles.container}>
+          {/* 로고 */}
           <div className={styles.logoSection}>
             <img src="/logo.svg" alt="LearnIT Logo" className={styles.logo} />
           </div>
 
+          {/* 메뉴 */}
           <div className={styles.menu}>
-            <NavLink to="/" className={({ isActive }) => `${styles.menuItem} ${isActive ? styles.active : ""}`}>홈</NavLink>
-            <NavLink to="/mygroup" className={({ isActive }) => `${styles.menuItem} ${isActive ? styles.active : ""}`}>내 그룹</NavLink>
-            <NavLink to="/mypage" className={({ isActive }) => `${styles.menuItem} ${isActive ? styles.active : ""}`}>마이페이지</NavLink>
+            <NavLink
+              to="/"
+              end
+              className={({ isActive }) =>
+                `${styles.menuItem} ${isActive ? styles.active : ""}`
+              }
+            >
+              홈
+            </NavLink>
+
+            <NavLink
+              to="/mygroup"
+              className={() =>
+                `${styles.menuItem} ${isMyGroupActive ? styles.active : ""}`
+              }
+            >
+              내 그룹
+            </NavLink>
+
+            <NavLink
+              to="/mypage"
+              className={({ isActive }) =>
+                `${styles.menuItem} ${isActive ? styles.active : ""}`
+              }
+            >
+              마이페이지
+            </NavLink>
           </div>
 
+          {/* 우측: 검색 + 로그인 */}
           <div className={styles.rightSection}>
-            <SearchBar placeholder="스터디 이름을 검색해 보세요" onSearch={emitSearch} />
-            <Link to="/login" className={styles.loginBtn}>로그인</Link>
+            <SearchBar
+              placeholder="스터디 이름을 검색해 보세요"
+              onSearch={emitSearch}
+            />
+            <Link to="/login" className={styles.loginBtn}>
+              로그인
+            </Link>
           </div>
         </div>
       </nav>
