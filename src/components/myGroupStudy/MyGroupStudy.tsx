3cccf2d35835233321603d7aa8fd0e753a378219
soyeon <soyeon20031010@gmail.com>
2025-08-17 14:55:46 +0900

    fix: study 부분 htmlcontent로 변경

--- src/components/myGroupStudy/MyGroupStudy.tsx
+++ src/components/myGroupStudy/MyGroupStudy.tsx
@@ -92,7 +92,7 @@ export default function MyGroupStudy() {
     <div className={styles.wrapper}>
       <div className={styles.noteBox}>
         <h2 className={styles.noteTitle}>{title}</h2>
-        <p className={styles.noteContent}>{content}</p>
+        <div className={styles.noteContent} dangerouslySetInnerHTML={{ __html: content }} />
       </div>
     </div>
   );
